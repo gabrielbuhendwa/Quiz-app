736f7c4fd7d3af7a6c646a3d2f2713af35057f0a
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-16 01:52:42 +0200

    add this to my repository

---
+++ assets/js/questions.js
@@ -0,0 +1,168 @@
+//setting up 15 sample questions for the quiz app
+export const questions = [
+    {
+        id: 1,
+        question: "Which language is used to style web pages?",
+        options: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        correctAnswer: 1
+    },
+    {
+        id: 2,
+        question: "Which language is used to structure web pages?",
+        options: [
+            "JavaScript",
+            "HTML",
+            "CSS",
+            "SQL"
+        ],
+        correctAnswer: 1
+    },
+    {
+        id: 3,
+        question: "Which language adds behavior to a webpage?",
+        options: [
+            "JavaScript",
+            "HTML",
+            "CSS",
+            "C++"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 4,
+        question: "How do you start a comment in JavaScript?",
+        options: [
+            "//",
+            "/*",
+            "#",
+            "<!--"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 5,
+        question: "How do you display text in the console?",
+        options: [
+            "console.log('text')",
+            "print('text')",
+            "echo('text')",
+            "log('text')"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 6,
+        question: "Which symbol is used for addition?",
+        options: [
+            "+",
+            "-",
+            "*",
+            "/"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 7,
+        question: "Which keyword declares a variable in JavaScript?",
+        options: [
+            "var",
+            "variable",
+            "letit",
+            "dim"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 8,
+        question: "How do you make a number 5 bigger by 1?",
+        options: [
+            "5 + 1",
+            "5 - 1",
+            "5 x 1",
+            "5 / 1"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 9,
+        question: "Which of these is an array?",
+        options: [
+            "{1, 2, 3}",
+            "[1, 2, 3]",
+            "(1, 2, 3)",
+            "<1, 2, 3>"
+        ],
+        correctAnswer: 1
+    },
+    {
+        id: 10,
+        question: "Which is used to create a function?",
+        options: [
+            "function myFunc() {}",
+            "func myFunc() {}",
+            "create myFunc() {}",
+            "make function myFunc() {}"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 11,
+        question: "How do you check equality in JavaScript?",
+        options: [
+            "==",
+            "=",
+            "===",
+            "equals"
+        ],
+        correctAnswer: 2
+    },
+    {
+        id: 12,
+        question: "Which keyword starts a loop?",
+        options: [
+            "for",
+            "loop",
+            "whilefun",
+            "repeat"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 13,
+        question: "Which is true about JavaScript?",
+        options: [
+            "It runs in the browser",
+            "It runs only on desktop apps",
+            "It is a database",
+            "It is a server hardware"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 14,
+        question: "How do you write a string?",
+        options: [
+            "'Hello'",
+            "Hello",
+            "{Hello}",
+            "[Hello]"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 15,
+        question: "Which method removes the last item from an array?",
+        options: [
+            "unshift()",
+            "push()",
+            "shift()",
+            "pop()"
+        ],
+        correctAnswer: 3
+    }
+];
